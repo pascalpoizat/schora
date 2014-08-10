de5f04573585a450802ee2716a1079bb451edc42
Pascal Poizat <pascal.poizat@lip6.fr>
2014-08-10 09:02:50 +0200

    add new tests for the parsing of events

--- src/fr/lri/schora/basicEvent/testCase/TestEventParser.java
+++ src/fr/lri/schora/basicEvent/testCase/TestEventParser.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * This code is distributed under GPL v2 licence.
- * 
+ *
  * This code is provided solely "as is". 
  * There is no warranty or other guarantee of fitness of this code.
  * The author disclaims all responsibility and liability 
  * with respect to this code's usage
  * or its effect upon hardware or computer systems.
- * 
+ *
  * author: Huu-Nghia Nguyen
  * email : nhnghia@me.com
  ******************************************************************************/
@@ -18,12 +18,48 @@ import fr.lri.schora.util.parser.ParserException;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class TestEventParser {
 
-	@Test
-	public void testParserString() throws ParserException {
-		Event e = EventReader.parser("request [a,b]");
-		System.out.println(e.toString());
-	}
+    @Test
+    public void testParserInteraction() {
+        try {
+            Event event = EventReader.parser("request [a,b] .");
+            assertEquals(event.toString(), "request[a,b].");
+        } catch (ParserException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testParserReception() {
+        try {
+            Event event = EventReader.parser("request [a,b] ?");
+            assertEquals(event.toString(), "request[a,b]?");
+        } catch (ParserException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testParserEmission() {
+        try {
+            Event event = EventReader.parser("request [a,b] !");
+            assertEquals(event.toString(), "request[a,b]!");
+        } catch (ParserException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testParserNOK() {
+        try {
+            Event event = EventReader.parser("request");
+            fail();
+        } catch (ParserException e) {
+            assertTrue(true);
+        }
+    }
 
 }
